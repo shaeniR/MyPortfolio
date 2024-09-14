b7efc57436c575780199370cfb1c63fdffb04b00
Shaeni <upekharasnayaka@gmail.com>
2024-09-14 11:07:53 +0530

    remove blog and update personal data

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
+    email: 'upekharasnayaka@gmail.com',
+    phone: '+94779995350',
+    address: 'Nikaweratiya,Kurunegala, Sri Lanka',
+    github: 'https://github.com/shaeniR',
+    facebook: 'https://web.facebook.com/upekha.shaeni',
+    linkedIn: 'https://www.linkedin.com/in/shaeni-rasnayaka-8563b8295/',
+    instagram: 'https://www.instagram.com/upekhashaeni/',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "said7388"
 }
