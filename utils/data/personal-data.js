41093fc0a74a1743011bd0df4f4aa4ea8ccd4c12
Shaeni <upekharasnayaka@gmail.com>
2024-09-22 22:37:12 +0530

    change projects and About me

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,10 +2,17 @@ export const personalData = {
   name: "SHAENI RASNAYAKA",
   profile: '/profile.png',
   designation: "IT UNDERGRADUATE",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  description: "My name is Shaeni Rasnayaka.I am an enthusiastic IT undergraduate\n" +
+      "at the University of Moratuwa. Also I\n" +
+      "am a self-motivated undergraduate\n" +
+      "with a passion for software\n" +
+      "development, seeking a position as a\n" +
+      "Software Engineer Intern to contribute\n" +
+      "to innovative projects and gain hands on experience in the field to enhance\n" +
+      "the growth of an organization.",
+  email: 'upekharasnayak@gmail.com',
+  phone: '+94779995350',
+  address: 'Kurunegala,Sri Lanka',
   github: 'https://github.com/shaeniR',
   facebook: 'https://web.facebook.com/upekha.shaeni',
   linkedIn: 'https://www.linkedin.com/in/shaeni-rasnayaka-8563b8295/',
